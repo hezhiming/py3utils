33a61067fc271597c9c8ed8fa22c174ec9741f2a
he.zhiming <he-zhiming@foxmail.com>
2018-02-20 12:02:14 +0800

    修改setup.py: 移除对外部库的依赖; 仅支持py3.5

--- setup.py
+++ setup.py
@@ -4,24 +4,18 @@ setup(name='py3utils',
       description='Useful Python 3.5 utils',
       version='0.1',
       url='https://github.com/hezhiming/py3utils',
-      author='hezhiming',
+      author='he.zhiming',
       author_email='he.zhiming@foxmail.com',
       license='MIT',
       classifiers=[
           'Intended Audience :: Developers',
           'Natural Language :: English',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: Implementation :: CPython',
       ],
       packages=['py3utils'],
-      install_requires=[
-          'python-dateutil>=2.5.0'
-      ],
       # entry_points={
       #     'console_scripts': [
       #         'encrypt=crytto.main:run'
