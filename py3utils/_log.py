1dec974693222864537b20b31ac33656bea92912
he.zhiming <he-zhiming@foxmail.com>
2018-04-15 23:39:50 +0800

    add LogFactory

---
+++ py3utils/_log.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# date:        2018/4/15
+# author:      he.zhiming
+#
+
+from __future__ import unicode_literals, absolute_import
+
+import logging
+import logging.config
+from logging import handlers
+
+
+class LogFactory:
+    _LOG_CONFIG_DICT = {
+
+    }
+
+    logging.config.dictConfig(_LOG_CONFIG_DICT)
+
+    @classmethod
+    def get_logger(cls, logger_name) -> logging.Logger:
+        return logging.getLogger(logger_name)
+
+
+DEBUGGER = LogFactory.get_logger('debugger')
+CONSOLE_LOGGER = LogFactory.get_logger('console_logger')
